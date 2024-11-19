31d1fb9f3d48bcf03a52b4388e16105dbb5c49b5
徐國堂 <roberthsu2003@gmail.com>
2024-11-19 12:04:26 +0000

    lesson9_03

--- lesson9/lesson9_1.py
+++ lesson9/lesson9_1.py
@@ -1,8 +1,8 @@
-import tools
+from gear import widget
 if __name__ == '__main__':
     nums = int(input('請輸入人數:'))
-    names:list[str] = tools.get_name(nums=nums)
-    students:list[dict] = tools.generate_bmi(names=names)
+    names:list[str] = widget.get_name(nums=nums)
+    students:list[dict] = widget.generate_bmi(names=names)
     for student in students:
         for key,value in student.items():
             print(f'{key}:{value}')
