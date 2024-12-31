5c4a904bec4c2e0230ea85ba147accb4c9b0c5c8
徐國堂 <roberthsu2003@gmail.com>
2024-12-31 13:02:46 +0000

    lesson21_1_6

--- lesson21/main.py
+++ lesson21/main.py
@@ -1,11 +1,16 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
 
 app = Flask(__name__)
+genai.configure(api_key=os.environ['Gemini_API_KEY'])
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 @app.route("/")
-@app.route("/<string:name>")
-def index(name:str=""):
-    return f"<h1>Hello,{name}您好!</h1>"
+@app.route("/<string:question>")
+def index(question:str=""):
+    return f"{question}"
 
 @app.route('/hello')
 def hello():
