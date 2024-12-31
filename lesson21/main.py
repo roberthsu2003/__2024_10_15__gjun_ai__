e46840f295ccadbacf7113374907c9c8249246c4
徐國堂 <roberthsu2003@gmail.com>
2024-12-31 11:44:07 +0000

    lesson21_main_4

--- lesson21/main.py
+++ lesson21/main.py
@@ -3,5 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return "<h1>Hello,徐國堂您好!</h1>"
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
